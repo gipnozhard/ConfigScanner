41ea2cc837662ed2ab66142d31feef55d693ae8e
Lavrentyev Pavel <pa.lavrentyev@gmail.com>
2026-04-21 23:29:24 +0300

    интерфейс правила

---
+++ internal/rules/rule.go
@@ -0,0 +1,12 @@
+package rules
+
+import "ConfigScanner/pkg/models"
+
+// Rule интерфейс для всех правил проверки
+type Rule interface {
+	// Name возвращает название правила
+	Name() string
+
+	// Check проверяет конфиг и возвращает проблемы
+	Check(config map[string]interface{}, path string) []models.Problem
+}
